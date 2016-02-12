0dd7f572d22e0877fabe9b772b109dc6dc6a773d
Noah Muth <noah.muth@code42.com>
2016-02-12 17:05:05 -0600

    WIP rendering a grid

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ extern crate piston_window;
 extern crate find_folder;
 
 use piston_window::*;
+use std::slice::from_raw_parts;
+
+#[derive(Copy, Clone)]
+struct Tile {
+    ch : char,
+    color : types::Color
+}
+
+#[derive(Copy, Clone)]
+struct Screen<'a> {
+    width : u32,
+    height : u32,
+    tiles : &'a[Tile]
+}
 
 fn main() {
     let window: PistonWindow =
@@ -21,28 +35,54 @@ fn main() {
     let black = [0.0, 0.0, 0.0, 1.0];
 
     let font_size = 14.0;
+    let w : u32 = 32;
+    let h : u32 = 32;
+    let screen_size = (w * h) as usize;
+    let default_tile = Tile{
+        ch: '.',
+        color: [0.0, 0.7, 0.0, 1.0]
+    };
+    let mut tiles : &[Tile] = &[default_tile];
 
-    let mut x = 0.0;
-    let mut y = 0.0;
+    unsafe {
+        tiles = from_raw_parts(&default_tile, screen_size);
+    }
+
+    let mut screen = Screen{
+        width: w,
+        height: h,
+        tiles: tiles
+    };
 
     for e in window {
         e.draw_2d(|c, g| {
             clear(black, g);
 
-            let transform = c.transform.trans(
-                x * font_size,
-                (y + 1.0) * font_size
-            );
+            let t = text::Text::new_color(white, 14);
 
-            text::Text::new_color(white, 14).draw(
-                "@",
-                &mut glyphs,
-                &c.draw_state,
-                transform,
-                g
-            );
+            for y in 0 .. screen.height {
+                let range_start = y * screen.width;
+                let range_end = range_start + screen.width;
+
+                for x in range_start .. range_end {
+                    let transform = c.transform.trans(
+                        (x as f64) * font_size,
+                        ((y as f64) + 1.0) * font_size
+                    );
+
+                    let idx = (y * screen.width + x) as usize;
+
+                    let tile = screen.tiles[idx];
+                    let s = ".";
+
+                    println!("about to render {} to {}, {}", s, x, y);
+                    t.draw(s, &mut glyphs, &c.draw_state, transform, g);
+                    println!("finished render to {}, {}", x, y);
+                }
+            }
         });
 
+        /*
         if let Some(Button::Keyboard(key)) = e.press_args() {
             match key {
                 Key::H => x -= 1.0,
@@ -52,5 +92,6 @@ fn main() {
                 _ => ()
             }
         }
+        */
     }
 }
